571f2712e7c80934e6412901f0e0229c887a22fc
Reginald Green <reggie.green10@yahoo.com>
2023-01-03 14:53:40 -0500

    "boolean compass"

---
+++ src/main/java/org/example/BooleanComparisons.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class BooleanComparisons {
+    public static void main(String[] args) {
+        int chemistryGrade = 95;
+        int biologyGrade = 75;
+        int englishGrade = 75;
+
+        System.out.println(englishGrade >= biologyGrade);
+
+        double sales = 37.55;
+        double costs = 5.55;
+
+        System.out.println( sales > costs);
+    }
+}
